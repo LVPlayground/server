2457970484d9ab9a5f6f34ac7806144c3bbdc661
Russell Krupke <russell@sa-mp.nl>
2015-10-17 23:14:58 +0100

    Correct init and .gitignore, add callbacks.txt

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 # Use of this source code is governed by the MIT license, a copy of which can
 # be found in the LICENSE file.
 
-ln -s ../playground/data server/data
-ln -s ../playground/javascript server/javascript
+ln -s ../playground/data data
+ln -s ../playground/javascript javascript
 
-ln -s ../echo-plugin/src/out/echo-plugin.so server/plugins/echo-plugin.so
-ln -s ../json-plugin/src/out/json-plugin.so server/plugins/json-plugin.so
+ln -s ../echo-plugin/src/out/echo-plugin.so plugins/echo-plugin.so
+ln -s ../json-plugin/src/out/json-plugin.so plugins/json-plugin.so
 # TODO: mysql-plugin.so
-ln -s ../playgroundjs-plugin/src/out/playgroundjs-plugin.so server/plugins/playgroundjs-plugin.so
-ln -s ../playgroundjs-plugin/src/out/libv8.so server/plugins/libv8.so
+ln -s ../playgroundjs-plugin/src/out/playgroundjs-plugin.so plugins/playgroundjs-plugin.so
+ln -s ../playgroundjs-plugin/src/out/libv8.so plugins/libv8.so
 # TODO: streamer.so
-ln -s ../zone-manager-plugin/src/out/zone-manager-plugin.so server/plugins/zone-manager-plugin.so
+ln -s ../zone-manager-plugin/src/out/zone-manager-plugin.so plugins/zone-manager-plugin.so
